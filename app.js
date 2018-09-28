50c3d3712f69c401ebf510ea43d67df63cf916d8
TuenTuenna <kor216465@gmail.com>
2018-09-28 11:37:38 +0000

    add mongolab

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp_v10",{ useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/yelp_camp_v10",{ useNewUrlParser: true });
+mongoose.connect("mongodb://Jeff:jeff216465@ds247699.mlab.com:47699/yelpcamp1234",{ useNewUrlParser: true });
+
 mongoose.set("useFindAndModify", false);
 
 app.use(bodyParser.urlencoded({ extended: true })); 
